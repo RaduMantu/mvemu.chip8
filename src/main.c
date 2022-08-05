96dc5d779f7d816ecba5d9cc56b88aca3071f7d5
Radu Mantu <andru.mantu@gmail.com>
2022-08-05 18:12:30 +0200

    Added GPLv3 license

--- src/main.c
+++ src/main.c
@@ -1,3 +1,22 @@
+/*
+ * Copyright © 2022, Radu-Alexandru Mantu <andru.mantu@gmail.com>
+ *
+ * This file is part of mvemu.chip8.
+ *
+ * mvemu.chip8 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * mvemu.chip8 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with mvemu.chip8. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include <stdint.h>     /* [u]int*_t       */
 
 #include "cli_args.h"
